f407b66e38e79f4e6de0c338a46aad0f5b39b416
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-04-05 02:40:22 +0600

    package json updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    jsxImportSource: '@emotion/react',
-    babel: {
-      plugins: ['@emotion/babel-plugin'],
-    },
-  })],
+  plugins: [react()],
+  server: {
+    port: 5173,
+  }
 })
